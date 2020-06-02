9536647e27b8d70b6bb7718abab41c07401eb47d
crannman <60367168+crannman@users.noreply.github.com>
2020-06-02 22:42:52 +0100

    Add files via upload

---
+++ emberrp/gamemode/init.lua
@@ -0,0 +1,19 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+
+include("shared.lua")
+
+-- Include Other Scripts
+
+function GM:PlayerSpawn(ply)
+  ply:SetGravity(.8)
+  ply:SetMaxHealth(100)
+  ply:SetRunSpeed(500)
+  ply:Give("weapon_physgun")
+  ply:SetupHands()
+
+end
+
+
+    end
---
+++ emberrp/gamemode/shared.lua
@@ -0,0 +1,14 @@
+GM.Name    = "EmbeRP"
+GM.Author  = "Crannman"
+GM.Email = "N/A"
+GM.Website = "N/A"
+
+DeriveGamemode("sandbox")
+
+
+function GM:Initialize()
+
+		self.BaseClass.Initialize(self)
+
+
+end
